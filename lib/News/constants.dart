faaba0563b8bb577fcff6997a39e8c9ff4442f1d
harshb-15 <hbhansali007@gmail.com>
2022-11-24 19:02:03 +0530

    harsh

--- lib/News/constants.dart
+++ lib/News/constants.dart
@@ -1,7 +1,7 @@
 class ApiConstants {
+  // Constants related to API
   static String baseUrl =
       "https://newsdata.io/api/1/news?apikey=pub_1299640f773c5d15f91f34225455168ed20c8&category=environment&language=en";
   static String page = "&page=";
   static String country = "&country=";
-
 }
